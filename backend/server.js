09e74d197011ea5200705bd23df23c71a1458f54
TariqVC <ST10248762@vcconnect.edu.za>
2026-04-06 22:52:12 +0200

    updated server.js for render run

--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,9 @@ app.use("/api/rooms", roomRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
+  app.get('/{*path}', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 }
 
 initSocket(httpServer);
